aa6ec0a089d5a0590d8ad9fb62782688f1fc5155
seoulection <6445410+seoulection@users.noreply.github.com>
2025-12-25 09:01:45 -0800

    adds jump to definition in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,16 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
-		vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live grep' })
-		vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope buffers' })
-		vim.keymap.set('n', '<leader>th', builtin.help_tags, { desc = 'Telescope help tags' })
-	end
+  'nvim-telescope/telescope.nvim',
+  tag = 'v0.2.0',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live grep' })
+    vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope buffers' })
+    vim.keymap.set('n', '<leader>th', builtin.help_tags, { desc = 'Telescope help tags' })
+    -- go to definition in vertical split
+    vim.keymap.set("n", "<leader>o", function()
+      builtin.lsp_definitions({ jump_type = "vsplit" })
+    end)
+  end
 }
